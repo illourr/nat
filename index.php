7330b5cdb6993276e29787cd022296ab8a21e61f
Dillon Curry <dilloncurry@gmail.com>
2013-09-27 17:46:37 -0500

    added h1 and pushing to herokuapp, headed out for the day, will continue tomorrow

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
+        <h1>Nerdery Assessment Test</h1>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
