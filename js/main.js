29e3492654bcbafbaec787c9596fd042d9c54c6f
Dillon Curry <dilloncurry@gmail.com>
2013-09-29 15:42:02 -0500

    finally working on the site, almost done with the top section going to start some ie testing as I go along

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+(function() {
+  "use strict";
+
+  $(function(){
+    // Legacy Browser Placeholder Fix.
+  	$('[placeholder]').focus(function() {
+  	  var input = $(this);
+  	  if (input.val() == input.attr('placeholder')) {
+  	    input.val('');
+  	    input.removeClass('placeholder');
+  	  }
+  	}).blur(function() {
+  	  var input = $(this);
+  	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+  	    input.addClass('placeholder');
+  	    input.val(input.attr('placeholder'));
+  	  }
+  	}).blur();
+    
+  })
+
+}());	
